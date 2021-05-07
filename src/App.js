6fc1aa2bb78d0613f1507b3e2f0550ff76f23e1b
JommieJom <jombhanut@gmail.com>
2021-05-07 16:19:05 +0700

    Register Login with js and scss

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Home from './components/pages/Home';
 import Course from './components/pages/Course';
 import Labs from './components/pages/Labs';
 import Footer from './components/Footer';
+import SignUp from './components/pages/SignUp';
 
 
 function App() {
@@ -17,7 +18,7 @@ function App() {
           <Route path='/' exact component={Home} />
           <Route path='/course-up' component={Course} />
           <Route path='/lab-list' component={Labs} />
-          
+          <Route path='/sign-up' component={SignUp} />
         </switch>
         <Footer />
       </Router>
