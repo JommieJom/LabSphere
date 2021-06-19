fce57c291462374c3cf74860d4e677333040abdf
UndetectedCheats <61540892+kasivit8@users.noreply.github.com>
2021-06-19 14:59:56 +0700

    markdown editor done

--- src/components/EditorComp.js
+++ src/components/EditorComp.js
@@ -12,30 +12,7 @@ const { Header, Content, Footer, Sider } = Layout;
 const { SubMenu } = Menu;
 const { Panel } = Collapse;
 
-const markdown = `
-## Lab1: Above Average
-It is said that 90% of frosh expect to be above average in their class. You are to provide a reality check.
-***
-## Input
-The first line of standard input contains an integer 1≤𝐶≤50, the number of test cases. 
-𝐶 data sets follow. Each data set begins with an integer, 𝑁, the number of people in the class (1≤𝑁≤1000). 
-𝑁 integers follow, separated by spaces or newlines, each giving the final grade (an integer between 0 and 100) of a student in the class.
-## Output
-For each case you are to output a line giving the percentage of students whose grade is above average, rounded to exactly 3 decimal places.
-### Sample Input 1
-5\n
-5 50 50 70 80 100\n
-7 100 95 90 80 70 60 50\n
-3 70 90 80\n
-3 70 90 81\n
-9 100 99 98 97 96 95 94 93 91\n
-### Sample Output 1
-40.000%\n
-57.143%\n
-33.333%\n
-66.667%\n
-55.556%\n
-`;
+const MARKDOWN_KEY = 'mdVal'
 
 class LabSelection extends React.Component {
     state = {
@@ -88,7 +65,7 @@ class LabSelection extends React.Component {
                             <div className="site-layout-background" style={{ padding: 24, minHeight: 360 }}>
                                 <Collapse defaultActiveKey={['Markdown', 'Code']}>
                                     <Panel header="Markdown" key="Markdown" style={{ textAlign: "left" }}>
-                                        <ReactMarkdown children={markdown} />
+                                        <ReactMarkdown children={localStorage.getItem(MARKDOWN_KEY)} />
                                     </Panel>
 
                                     <Panel header="Code" key="Code">
--- src/components/MarkdownEditor.js
+++ src/components/MarkdownEditor.js
@@ -2,16 +2,44 @@ import React from 'react';
 import './MarkdownEditor.css';
 import { Remarkable } from 'remarkable';
 
+const markdown = `
+## Lab1: Above Average
+It is said that 90% of frosh expect to be above average in their class. You are to provide a reality check.
+***
+## Input
+The first line of standard input contains an integer 1≤𝐶≤50, the number of test cases. 
+𝐶 data sets follow. Each data set begins with an integer, 𝑁, the number of people in the class (1≤𝑁≤1000). 
+𝑁 integers follow, separated by spaces or newlines, each giving the final grade (an integer between 0 and 100) of a student in the class.
+## Output
+For each case you are to output a line giving the percentage of students whose grade is above average, rounded to exactly 3 decimal places.
+### Sample Input 1
+5\n
+5 50 50 70 80 100\n
+7 100 95 90 80 70 60 50\n
+3 70 90 80\n
+3 70 90 81\n
+9 100 99 98 97 96 95 94 93 91\n
+### Sample Output 1
+40.000%\n
+57.143%\n
+33.333%\n
+66.667%\n
+55.556%\n
+`;
+
+const MARKDOWN_KEY = 'mdVal'
+
 class MarkdownEditor extends React.Component {
     constructor(props) {
       super(props);
       this.md = new Remarkable();
       this.handleChange = this.handleChange.bind(this);
-      this.state = { value: '## Java Lab1: ' };
+      this.state = { value: localStorage.getItem(MARKDOWN_KEY) || markdown};
     }
   
     handleChange(e) {
-      this.setState({ value: e.target.value });
+      localStorage.setItem(MARKDOWN_KEY, e.target.value);
+      this.setState({ value: localStorage.getItem(MARKDOWN_KEY) });
     }
   
     getRawMarkup() {
